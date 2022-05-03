d108c94715b70578a0eec9d2f5bec918b189567d
Al-Imran <alimran21feb@gmail.com>
2022-05-03 22:47:57 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -7,6 +8,20 @@ require('dotenv').config();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// Username= dbuser
+// password= gYKkyJDGQNJqA3U7
+
+// 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cje0k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('Fruits Warehouse DB Connected');
+  // perform actions on the collection object
+  client.close();
+});
+
 
 // 
 app.get('/', (req, res) => {
